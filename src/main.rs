f8fd23e743852e824f7006c6ebbcb83db1f60bf0
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2022-02-09 19:51:27 +0600

    Added constructor to config, and error check for lesser params

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,26 @@
 
 use std::env;
 use std::fs;
+use std::process;
 
 struct Config {
     query: String,
     filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let conf: Config = Config{
+            query: args[1].clone(),
+            filename: args[2].clone()
+        };
+
+        return Ok(conf);
+    }
 }
 
 fn main() {
@@ -13,7 +29,11 @@ fn main() {
 
     println!("{:?}", args); 
 
-    let conf = parseParams(&args);
+    let conf = Config::new(&args)
+            .unwrap_or_else(|err| {
+                println!("Problem parsing arguments: {}", err);
+                process::exit(1);
+            });
 
     let contents = fs::read_to_string(conf.filename) 
                     .expect("could not read file.");
@@ -21,11 +41,3 @@ fn main() {
     println!("With text:\n{}", contents);
 }
 
-
-fn parseParams(args: &[String]) -> Config {
-    // TODO check length
-    return Config{
-        query: args[1].clone(),
-        filename: args[2].clone()
-    };
-}
